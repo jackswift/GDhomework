656e143660cfe5cd3412635880bc04b20eed27f4
Lyle <lmt399@nyu.edu>
2016-02-07 19:47:48 -0500

    Finished hw1

--- NYUCodebase/main.cpp
+++ NYUCodebase/main.cpp
@@ -14,12 +14,15 @@
 #define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
 #endif
 
+float MaxYPos = 2.0f;
+float MaxXPos = 3.55f;
 
 GLuint LoadTexture(const char *image_path);
 void setBackgroundColorAndClear();
 void setProgramMatrices(ShaderProgram &program, Matrix projectionMatrix, Matrix modelMatrix, Matrix viewMatrix);
 SDL_Window* displayWindow;
 
+void drawTexture(GLuint texture, ShaderProgram program);
 
 
 int main(int argc, char *argv[])
@@ -37,22 +40,36 @@ int main(int argc, char *argv[])
 #endif
     
     glViewport(0, 0, 640, 360);
-    
     //Setup program and initialize matrixes
-    ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_texured.glsl");
-    
-    //create texture to be bound for display
+    ShaderProgram program(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
     
     
-    //Initialize all the matrices necessary
+    GLuint smileyFace = LoadTexture("Smiley.png");
+    GLuint kittenPic = LoadTexture("kitten.png");
+    GLuint someImg = LoadTexture("someImg.png");
+    
     Matrix projectionMatrix;
     Matrix modelMatrix;
+    Matrix modelMatrix2;
+    Matrix modelMatrix3;
     Matrix viewMatrix;
-    glUseProgram(program.programID);
     
+    //positions of textures
+    float lastFrameticks = 0.0f;
+    float smileyX = 1.0f;
+    float smileyY = 0.0f;
+    float kittenX = -1.0f;
+    float kittenY = 0.0f;
+    float someImgX = 0.0f;
+    float someImgY = 0.0f;
+    float someImgRotation = 0.0f;
     //set ortho projection of the projectionMatrix (changing these values will set the cartesian
     //plane units of the game screen
     projectionMatrix.setOrthoProjection(-3.55f, 3.55f, -2.0f, 2.0f, -1.0f, 1.0f);
+    
+    
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     
     SDL_Event event;
     bool done = false;
@@ -62,16 +79,55 @@ int main(int argc, char *argv[])
                 done = true;
             }
         }
-        setBackgroundColorAndClear();
-        setProgramMatrices(program, projectionMatrix, modelMatrix, viewMatrix);
+        float ticks = (float)SDL_GetTicks() / 1000.0f;
+        float elapsed = ticks - lastFrameticks;
+        lastFrameticks = ticks;
+        //setProgramMatrices(program, projectionMatrix, modelMatrix, viewMatrix);
         
-        float vertices[] = {0.5f, -0.5f, 0.0f, 0.5f, -0.5f, -0.5f};
+        smileyX += elapsed;
+        if(smileyX >= (MaxXPos-0.5f))
+        {
+            smileyX = 1.0f;
+        }
+        kittenX -= elapsed;
+        if(kittenX <= (-MaxXPos+0.5f))
+        {
+            kittenX = -1.0f;
+        }
+        someImgY += elapsed*2;
+        if(someImgY >= (MaxYPos-0.5f))
+        {
+            someImgY = -1.5f;
+        }
+        someImgRotation += elapsed*80;
+        std::cout << someImgRotation << std::endl;
         modelMatrix.identity();
-        modelMatrix.Translate(1.0f, 0.0f, 0.0f);
-        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
-        glEnableVertexAttribArray(program.positionAttribute);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDisableVertexAttribArray(program.positionAttribute);
+        modelMatrix.Translate(smileyX, smileyY, 0.0f);
+        modelMatrix2.identity();
+        modelMatrix2.Translate(kittenX, kittenY, 0.0f);
+        modelMatrix3.identity();
+        modelMatrix3.Translate(someImgX, someImgY, 0.0f);
+        modelMatrix3.Rotate(someImgRotation * (3.14159/180.0f));
+        
+        program.setModelMatrix(modelMatrix);
+        program.setViewMatrix(viewMatrix);
+        program.setProjectionMatrix(projectionMatrix);
+        
+        
+        setBackgroundColorAndClear();
+        
+        glUseProgram(program.programID);
+        
+        drawTexture(smileyFace, program);
+        
+        program.setModelMatrix(modelMatrix2);
+        
+        drawTexture(kittenPic, program);
+        
+        program.setModelMatrix(modelMatrix3);
+        
+        drawTexture(someImg, program);
+        
         SDL_GL_SwapWindow(displayWindow);
     }
     
@@ -85,12 +141,14 @@ void setBackgroundColorAndClear()
     glClear(GL_COLOR_BUFFER_BIT);
 
 }
+
 void setProgramMatrices(ShaderProgram &program, Matrix projectionMatrix, Matrix modelMatrix, Matrix viewMatrix)
 {
     program.setModelMatrix(modelMatrix);
     program.setViewMatrix(viewMatrix);
     program.setProjectionMatrix(projectionMatrix);
 }
+
 GLuint LoadTexture(const char *image_path)
 {
     SDL_Surface *surface = IMG_Load(image_path);
@@ -99,7 +157,7 @@ GLuint LoadTexture(const char *image_path)
     
     glBindTexture(GL_TEXTURE_2D, textureID);
     
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_BGRA, surface->w, surface->h, 0, GL_BGRA, GL_UNSIGNED_BYTE, surface->pixels);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w, surface->h, 0, GL_BGRA, GL_UNSIGNED_BYTE, surface->pixels);
     
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -107,4 +165,32 @@ GLuint LoadTexture(const char *image_path)
     SDL_FreeSurface(surface);
     
     return textureID;
+}
+
+
+void drawTexture(GLuint theTexture, ShaderProgram program)
+{
+    //Matrix modelMatrix;
+    //Matrix viewMatrix;
+    
+    
+    
+    //modelMatrix.identity();
+    //modelMatrix.Translate(x, y, 0.0f);
+    
+    float vertices[] = {-0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f};
+    
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    //std::cout << program.positionAttribute << std::endl;
+    float texCoords[] = {0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0};
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+    
+    glBindTexture(GL_TEXTURE_2D, theTexture);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    glDisableVertexAttribArray(program.positionAttribute);
+    glDisableVertexAttribArray(program.texCoordAttribute);
+    
 }
